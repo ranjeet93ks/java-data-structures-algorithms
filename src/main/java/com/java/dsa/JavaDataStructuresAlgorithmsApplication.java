2dd07e9e15a687fc7f5958728a71ca272603b244
Ranjeet Kumar <ranjeet93ks@gmail.com>
2023-04-02 23:16:12 +0530

    first commit

---
+++ src/main/java/com/java/dsa/JavaDataStructuresAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.java.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDataStructuresAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDataStructuresAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/dsa/JavaDataStructuresAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDataStructuresAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
